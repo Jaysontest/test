d337ec1c110ae7a4c2c6be2f8b19cd5f19cd28c3
1 <1@163.com>
2016-10-23 21:17:16 +0800

    git checkout

--- bios.c
+++ bios.c
@@ -34,5 +34,49 @@ git log 或 git log --graph --pretty=oneline
 简短形式的git log --graph --pretty=oneline --abbrev-commit
 当需要版本回滚的时候 特别重要
 
-*/
+
+配合git git log --graph --pretty=oneline --abbrev-commit使用，可以方便回滚
+git reset --hard 238a90a(版本的前七个数)--绝对回退
+
+--相对回退
+或git reset --hard HEAD^ ^代表上个版本个数 ^^ ^^^ ^^^^ ....
+还可以使用git reset HEAD~100 回到上100个版本
+
+我们都知道HEAD默认指向当前分支的最新历史版本，而git给了我们一种以HEAD方式的相对语句就是HEAD^ 这个代表当前版本的上一个版本，HEAD^^ 代表当前版本的上上一个版本。
+
+*********注意：*************
+当我们使用回退命令之后就无法查看当前版本的信息 
+这时用：git reflog git log是版本历史记录，git reflog是命令历史记录。从这个里面就可以找到最新版本的commitid，就可以回退到任意一个版本。
+
+*yson@DESKTOP-86U6H0H MINGW32 ~/Desktop/pro/test (master)
+$ git log --graph --pretty=oneline --abbrev-commit
+* 3fa92a0 git log
+* e7c2f68 git diff
+* 8999fc5 详细的操作命令
+* 00e2e18 modified test2
+* ce189a7 Update bios.c
+* 48b567c Create bios.c
+* 785f03e Initial commit
+
+Jayson@DESKTOP-86U6H0H MINGW32 ~/Desktop/pro/test (master)
+$ vi bios.c
+
+Jayson@DESKTOP-86U6H0H MINGW32 ~/Desktop/pro/test (master)
+$ git reflog
+3fa92a0 HEAD@{0}: reset: moving to 3fa92a0
+ab4f8dc HEAD@{1}: commit: test git reset
+3fa92a0 HEAD@{2}: commit: git log
+e7c2f68 HEAD@{3}: commit: git diff
+8999fc5 HEAD@{4}: commit: 详细的操作命令
+00e2e18 HEAD@{5}: clone: from https://github.com/Jaysontest/test.git
+/
+
+
+**********************************************************************************
+这几个命令慎用：
+git checkout -- filename:将工作区下的文件恢复到暂存区下的状态：取消本地为暂存的修改
+暂存区不会被删除；
+
+git reset HEAD filename：将暂存区的修改恢复到本地工作区，暂存区将被清空。
+
 
