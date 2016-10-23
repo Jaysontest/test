8999fc5384f5044d9a99cf74dda3f0d4dfdee973
1 <1@163.com>
2016-10-23 19:22:59 +0800

    详细的操作命令

--- bios.c
+++ bios.c
@@ -3,3 +3,26 @@ main(){
 scanf("new file %s");
 printf("creadt finish!  \n");
 }
+/*
+1.git config --global user.name 'Jasyon'
+  git config --global user.email '1@163.com'
+ 初始化仓库
+  mkdir test
+  cd test
+  git init
+
+  touch test.c//在工作区 
+  git add -A / git add . 添加全部的文件//将文件添加到 暂存区
+  git commit -m 'descreption ' 提交到本地仓库中
+
+2.与远程仓库建立连接
+git remote add origin https://github.com/Jaysontest/test.git //github已存在的账户
+
+git clone https://github.com/Jaysontest/test.git
+
+3.提交到远程仓库
+git push -u origin master // -u 第一次提交记住本地仓库与远程仓库的连接 以后不要重新连接
+
+
+
+*/
